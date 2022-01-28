8bcc5b41ddc7d4d1bea983bd9939b58493d992c9
Diogo Souza <bdiogo212@gmail.com>
2022-01-28 20:57:08 -0300

    aaaa

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -8,8 +8,9 @@ class LogController extends Controller
 {
     public function soma(Request $resquest)
     {
-    $result=$resquest->$num1+$num2;
+    $result=$resquest->$num1+$resquest->$num2;
     logger()->info('Soma feita');
     return $result;
     }
+
 }
